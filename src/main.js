7ed872d029b63867c97cdcedeee8d6b80e1df6e0
Paula <paulare17@gmail.com>
2025-02-11 12:41:52 +0100

    Guardat

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 
-document.querySelector('#app').innerHTML = `
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const closeBtn = document.querySelector('#closeBtn'); //potser és un punt i no un #
+  const navbarToggler = document.querySelector('.navbar-toggler');
   
-`
+  navbarToggler.addEventListener('click', function() {
+      document.querySelector('.navbar-collapse').classList.toggle('show');
+  });
+  
+  closeBtn.addEventListener('click', function() {
+      document.querySelector('.navbar-collapse').classList.remove('show');
+  });
+});
